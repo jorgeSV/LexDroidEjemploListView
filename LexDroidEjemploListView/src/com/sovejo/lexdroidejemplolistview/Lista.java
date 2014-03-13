ba45013d6f6607effedc6a03b12b140e2241e17a
jorgeSV <amk114@gmail.com>
2014-03-13 18:28:43 +0100

    Ejemplo terminado.

---
+++ LexDroidEjemploListView/src/com/sovejo/lexdroidejemplolistview/Lista.java
@@ -0,0 +1,38 @@
+package com.sovejo.lexdroidejemplolistview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Lista extends ArrayAdapter<String> 
+{
+	private final Context context;
+	private final String[] animales;
+	
+	public Lista(Context context, String[] animales) 
+	{
+		super(context, R.layout.elemento, animales);
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		this.animales = animales;
+	}
+
+	public View getView(int position, View converView, ViewGroup parent)
+	{
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		View rowView = inflater.inflate(R.layout.elemento, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.textView01);
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.logo01);
+		
+		textView.setText(animales[position]);
+		imageView.setImageResource(R.drawable.ic_launcher);
+		
+		return rowView;
+	}
+	
+}
--- LexDroidEjemploListView/src/com/sovejo/lexdroidejemplolistview/MainActivity.java
+++ LexDroidEjemploListView/src/com/sovejo/lexdroidejemplolistview/MainActivity.java
@@ -1,16 +1,33 @@
 package com.sovejo.lexdroidejemplolistview;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 
-public class MainActivity extends Activity 
-{
-
+public class MainActivity extends ListActivity 
+{ 
+	static final String[] animales01 = {"Perro", "Gato", "Caballo", "Conejo", "León", "Tigre", "Serpiente", "Cocodrilo", "Hipopótamo", "Ratón"};
+	
+	//Public en lugar de static para poder usar el contexto (this)
+	/*static final String[] animales01 = 
+		{
+			this.getResources().getString(R.string.perro),
+			this.getResources().getString(R.string.gato),
+			this.getResources().getString(R.string.caballo),
+			this.getResources().getString(R.string.conejo),
+			this.getResources().getString(R.string.leon),
+			this.getResources().getString(R.string.tigre),
+			this.getResources().getString(R.string.cocodrilo),
+			this.getResources().getString(R.string.hipopotamo),
+			this.getResources().getString(R.string.raton),
+		};
+	*/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		
+		setListAdapter(new Lista(this, animales01));
 	}
 
 }
